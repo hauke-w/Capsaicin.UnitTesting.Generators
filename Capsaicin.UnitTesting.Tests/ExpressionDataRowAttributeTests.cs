9f3d8216c77babbf4bc0e5bedaf4e524fa3817a5
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-03-28 13:12:33 +0200

    added tests

---
+++ Capsaicin.UnitTesting.Tests/ExpressionDataRowAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capsaicin.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capsaicin.UnitTesting
+{
+    [TestClass]
+    public class ExpressionDataRowAttributeTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            object?[] parameters = null!;
+            Assert.ThrowsException<ArgumentNullException>(() => new ExpressionDataRowAttribute(parameters));
+
+            parameters = new object?[] { };
+            var actual = new ExpressionDataRowAttribute(parameters);
+            Assert.AreSame(parameters, actual.Parameters);
+
+            parameters = new object?[] { new object() };
+            actual = new ExpressionDataRowAttribute(parameters);
+            Assert.AreSame(parameters, actual.Parameters);
+        }
+    }
+}
