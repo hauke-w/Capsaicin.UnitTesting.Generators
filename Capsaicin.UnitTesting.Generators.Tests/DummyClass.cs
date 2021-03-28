1c41ea406840d8efd67008c89095f163917f54e5
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-03-28 15:59:15 +0200

    added test project

---
+++ Capsaicin.UnitTesting.Generators.Tests/DummyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsaicin.UnitTesting.Generators.Tests
+{
+    public class DummyClass<TValue> : IFormattable
+        where TValue : IFormattable
+    {
+        public DummyClass(string name, TValue value)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Value = value;
+        }
+
+        public string Name { get; }
+        public TValue Value { get; }
+
+        public override string ToString() => $"{Name}: {Value}";
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return $"{Name}: {Value.ToString(format, formatProvider)}";
+        }
+    }
+}
---
+++ Capsaicin.UnitTesting.Generators.Tests/TestDataSourceGeneratorTest.cs
@@ -0,0 +1,47 @@
+using Capsaicin.UnitTesting;
+using Capsaicin.UnitTesting.Generators.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization; // this namespace must be included because CultureInfo is used in an expression!
+
+namespace Capsaicin.UnitTesting.Generators
+{
+    [TestClass]
+    public partial class TestDataSourceGeneratorTest
+    {
+        private static DummyClass<double> CreateObject3() => new DummyClass<double>("Object3", Math.Pow(3, 4));
+
+        private static IFormattable[] Objects = new[]
+        {
+            (IFormattable)new DummyClass<double>("Object4", Math.PI),
+            new DummyClass<DummyClass<double>>("Object5", new DummyClass<double>("nested", 123.456)),
+        };
+
+        /// <summary>
+        /// Implicitly tests TestDataSourceGenerator
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="testObject"></param>
+        /// <param name="format"></param>
+        /// <param name="formatProvider"></param>
+        [TestMethod]
+        [ExpressionDataRow("Object1: -123,456.00", "new DummyClass<int>(\"Object1\", -123456)", "N2", "null")]
+        [ExpressionDataRow("Object2: 1.414214", "new DummyClass<double>(\"Object2\", Math.Sqrt(2))", "F6", null)]
+        // expression may contain method call, which is usefull for complex objects:
+        [ExpressionDataRow("Object3: 81", "CreateObject3()", null, "CultureInfo.InvariantCulture")]
+        // expression may contain reference to static field or property:
+        [ExpressionDataRow("Object4: 3,142", "Objects[0]", "F3", "CultureInfo.GetCultureInfo(\"de-DE\")")]
+        [ExpressionDataRow("Object5: nested: 123.456", "Objects[1]", "", "CultureInfo.GetCultureInfo(\"en-GB\")")]
+        public partial void GenerateTest(string expected, // not annotated with FromCSharpExpression because values are expressed with constant expressions
+            [FromCSharpExpression] IFormattable testObject,
+            string? format, // not annotated with FromCSharpExpression because values are expressed with constant expressions
+            [FromCSharpExpression] IFormatProvider? formatProvider)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-GB");
+            Assert.AreEqual(testObject.GetType().GetGenericTypeDefinition(), typeof(DummyClass<>), $"Invalid test data. {nameof(testObject)} is not typeof {typeof(DummyClass<>).FullName}.");
+            var actual = testObject.ToString(format, formatProvider);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
